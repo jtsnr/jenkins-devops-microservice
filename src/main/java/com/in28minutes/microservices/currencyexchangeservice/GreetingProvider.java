5202f4d19c9ff4f62de5c1752838b0866cbbadf0
James Turnbull <jsturnbull@hotmail.com>
2021-03-03 08:42:26 +0000

    Add informal greeting method

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/GreetingProvider.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/GreetingProvider.java
@@ -5,8 +5,18 @@ import java.util.Objects;
 public class GreetingProvider {
     private static final String TEMPLATE = "Hello, %s";
 
+    private static final String INFORMAL_TEMPLATE = "Hi there, %s";
+
     public String getGreeting(String name) {
         Objects.requireNonNull(name, "No name provided");
         return String.format(TEMPLATE, name);
+    }
+
+    public String getInformalGreeting(String name) {
+        return String.format(INFORMAL_TEMPLATE, name);
+    }
+
+    public String getDefaultGreeting() {
+        return getGreeting("World");
     }
 }
