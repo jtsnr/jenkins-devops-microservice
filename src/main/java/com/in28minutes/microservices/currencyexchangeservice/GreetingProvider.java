9abe207656713fc568cec4d80e8166f84247216c
James Turnbull <jsturnbull@hotmail.com>
2021-03-03 08:25:00 +0000

    Handle no name in greeting provider

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/GreetingProvider.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/GreetingProvider.java
@@ -4,6 +4,9 @@ public class GreetingProvider {
     private static final String TEMPLATE = "Hello, %s";
 
     public String getGreeting(String name) {
+		if (name == null) {
+			name = "World";
+		}
         return String.format(TEMPLATE, name);
     }
 }
