5c244ac8a0c4238f9309448752c4c29e62b2b930
James Turnbull <jsturnbull@hotmail.com>
2021-03-03 08:26:45 +0000

    Check a name is provided

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/GreetingProvider.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/GreetingProvider.java
@@ -1,9 +1,12 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import java.util.Objects;
+
 public class GreetingProvider {
     private static final String TEMPLATE = "Hello, %s";
 
     public String getGreeting(String name) {
+        Objects.requireNonNull(name, "No name provided");
         return String.format(TEMPLATE, name);
     }
 }
